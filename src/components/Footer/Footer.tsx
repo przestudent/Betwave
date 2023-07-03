d39ef495fb6860445ff4dce76108894fdbf844ff
3 <przemekpalac@student.agh.edu.pl>
2023-07-03 17:45:36 +0200

    pls work

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -151,7 +151,7 @@ const Footer: FC = async () => {
     </>
   );
 };
-CreateMatch();
+// CreateMatch();
 async function CreateMatch() {
   const count = await prisma.match.count();
   if (count === 2) {
